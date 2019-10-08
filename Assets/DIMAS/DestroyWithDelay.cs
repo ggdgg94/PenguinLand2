426250ad5739a6f5f94e8464d5ca6266fdb45888
dimas <46415558+Dimas713@users.noreply.github.com>
2019-10-08 11:33:53 -0500

    added Scripts
    added, scripts

---
+++ Assets/DIMAS/DestroyWithDelay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWithDelay : MonoBehaviour
+{
+    public GameObject destroyEffect;
+    public float delay;
+    // Start is called before the first frame update
+    void destroyAndEffect()
+    {
+
+        Instantiate(destroyEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/DIMAS/bulletMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletMovement : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    GameObject destroyEffect;
+    public int damage = -10;
+
+
+    // adds spped the the bullets movements
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+
+    //detects collision and applies damage to the enemy it also creates
+    //an effect when enemy is destroyed
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Debug.Log(hitInfo.name);
+
+        enemyLife enemy = hitInfo.GetComponent<enemyLife>();
+        if(enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+
+        Instantiate(destroyEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+
+        
+    }
+}
